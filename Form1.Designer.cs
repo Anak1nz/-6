39eb1178fd49d8b96a2cd453d8def8de967127b6
Vova <108748554+Mrkurort@users.noreply.github.com>
2024-12-21 10:29:43 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Работа_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(253, 143);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Заказчик";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(363, 144);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(78, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Сотрудник";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(234, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Форма регистрации";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Категория*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(175, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Фамилия*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(175, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Имя*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(175, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Отчество*";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(250, 285);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Ваш логин";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(426, 285);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Ваш пароль";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(515, 148);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 39);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Дата\r\nРождения*\r\n\r\n";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.Visible = false;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(610, 152);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 11;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            this.maskedTextBox1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(253, 177);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(253, 215);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(253, 250);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(316, 282);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(508, 282);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(515, 187);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Номер телефона*";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Visible = false;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(518, 215);
+            this.maskedTextBox2.Mask = "+7(999) 000-0000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox2.TabIndex = 18;
+            this.maskedTextBox2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.CausesValidation = false;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(508, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Сгенерировать логин/пароль";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Форма регистрации";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Работа_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                // Показываем элементы для radioButton1
+                label9.Visible = true;
+                maskedTextBox2.Visible = true;
+
+                // Скрываем элементы для radioButton2
+                label8.Visible = false;
+                maskedTextBox1.Visible = false;
+            }
+        }
+        private void radioButton2_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                // Показываем элементы для radioButton2
+                label8.Visible = true;
+                maskedTextBox1.Visible = true;
+
+                // Скрываем элементы для radioButton1
+                label9.Visible = false;
+                maskedTextBox2.Visible = false;
+            }
+        }
+         private void button1_Click(object sender, EventArgs e)
+        {
+            string First = textBox1.Text;
+            string Second = textBox2.Text;
+            string Last = textBox3.Text;
+            string Phone = maskedTextBox2.Text.Trim();
+            string Birth = maskedTextBox1.Text.Trim();
+
+            // Проверка пустых значений для имени, фамилии и отчества
+            if (string.IsNullOrWhiteSpace(First) || string.IsNullOrWhiteSpace(Second) || string.IsNullOrWhiteSpace(Last))
+            {
+                MessageBox.Show("Все поля должны быть заполнены.");
+                return;
+            }
+
+            string login = string.Empty;
+
+            // Генерация логина заказчика, если выбран radioButton1
+            if (radioButton1.Checked)
+            {
+                // Номер телефона обязателен для заказчика
+                if (string.IsNullOrWhiteSpace(Phone) || Phone.Length < 3)
+                {
+                    MessageBox.Show("Номер телефона должен быть заполнен и содержать хотя бы 3 цифры.");
+                    return;
+                }
+
+                login = GenerateCustomerLogin(First, Second, Last, Phone);
+                textBox4.Text = login;
+            }
+
+            // Генерация логина сотрудника, если выбран radioButton2
+            if (radioButton2.Checked)
+            {
+                // Дата рождения не обязательна для сотрудника, но если она есть, она используется
+                login = GenerateEmployeeLogin(First, Second, Last, Birth);
+                textBox4.Text = login;
+            }
+
+            // Генерация пароля
+            string password = GeneratePassword();
+            textBox5.Text = password;
+        }
+
+        // Генерация логина для сотрудника
+        private string GenerateEmployeeLogin(string firstName, string lastName, string middleName, string birthDate)
+        {
+            // Проверка обязательных полей для сотрудника
+            if (string.IsNullOrWhiteSpace(lastName) || string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(middleName))
+            {
+                MessageBox.Show("Все поля для сотрудника должны быть заполнены.");
+                return string.Empty;
+            }
+
+            try
+            {
+                string login = lastName[0].ToString().ToUpper() +
+                               firstName[0].ToString().ToUpper() +
+                               middleName[0].ToString().ToUpper();
+
+                // Получаем последние 2 цифры года рождения из строки
+                if (!string.IsNullOrWhiteSpace(birthDate) && birthDate.Length >= 10)
+                {
+                    string yearOfBirth = birthDate.Substring(6, 4);  // Год рождения (yyyy)
+                    string yearSuffix = yearOfBirth.Substring(2, 2); // последние две цифры года
+                    login += yearSuffix;
+                }
+
+                return login;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при генерации логина для сотрудника: " + ex.Message);
+                return string.Empty;
+            }
+        }
+
+        // Генерация логина для заказчика
+        private string GenerateCustomerLogin(string firstName, string lastName, string middleName, string phone)
+        {
+            // Проверка обязательных полей для заказчика
+            if (string.IsNullOrWhiteSpace(lastName) || string.IsNullOrWhiteSpace(firstName) ||
+                string.IsNullOrWhiteSpace(middleName) || string.IsNullOrWhiteSpace(phone))
+            {
+                MessageBox.Show("Все поля для заказчика должны быть заполнены.");
+                return string.Empty;
+            }
+
+            
+                string login = lastName[0].ToString().ToUpper() +firstName[0].ToString().ToUpper() + middleName[0].ToString().ToUpper();
+                // Получаем последние 3 цифры номера телефона
+                string phoneSuffix = phone.Length >= 3 ? phone.Substring(phone.Length - 3, 3) : phone;
+                login += phoneSuffix;
+                return login;
+        }
+
+        private string GeneratePassword()
+        {
+            Random random = new Random();
+
+            // 2 строчные буквы
+            char firstChar = (char)random.Next('a', 'z' + 1);
+            char secondChar = (char)random.Next('a', 'z' + 1);
+
+            // 4 заглавные буквы
+            char thirdChar = (char)random.Next('A', 'Z' + 1);
+            char fourthChar = (char)random.Next('A', 'Z' + 1);
+            char fifthChar = (char)random.Next('A', 'Z' + 1);
+            char sixthChar = (char)random.Next('A', 'Z' + 1);
+
+            // 2 случайные цифры
+            char seventhChar = (char)random.Next('0', '9' + 1);
+            char eighthChar = (char)random.Next('0', '9' + 1);
+
+            // Формирование пароля в нужном формате
+            return $"{firstChar}{secondChar}{thirdChar}{fourthChar}{seventhChar}{eighthChar}{fifthChar}{sixthChar}";
+        }
+    }
+}
+
